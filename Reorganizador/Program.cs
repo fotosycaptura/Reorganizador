cfbadc105287946d9c1f9bca4e48ab7a31ab0d28
Xavier Fuentes <charsxavier@gmail.com>
2021-02-05 20:37:56 -0300

    Se agrega opción de creación de carpeta por fecha de modificación en caso de no encontrase el tag de fecha de captura en Exif

--- Reorganizador/Program.cs
+++ Reorganizador/Program.cs
@@ -13,7 +13,7 @@ namespace Reorganizador
             string[] files = Directory.GetFiles(".");
             foreach (var item in files)
             {
-                Console.WriteLine(item.ToString());
+                Console.WriteLine("Procesando: " + item.ToString());
                 FileInfo fi = new FileInfo(item);
                 if (fi.Extension.ToLower().Equals(".cr2") || fi.Extension.ToLower().Equals(".nef")|| fi.Extension.ToLower().Equals(".jpg"))
                 {
@@ -23,10 +23,12 @@ namespace Reorganizador
                     {
                         for (int i=0; i < infoExif.Count; i++)
                         {
+                            bool blTagEncontrado = false;
                             for (int j=0; j < infoExif[i].Tags.Count; j++)
                             {
                                 if (infoExif[i].Tags[j].Name.Equals("Date/Time"))
                                 {
+                                    blTagEncontrado = true;
                                     string strFecha = infoExif[i].Tags[j].Description;
                                     strFecha = strFecha.Replace(":", "_");
                                     string[] yyyyMMdd = strFecha.Split(" ");
@@ -43,6 +45,21 @@ namespace Reorganizador
                                     break;
                                 }//if
                             }//for
+                            if (!blTagEncontrado)
+                            {
+                                Console.WriteLine("Tag de fecha no encontrado, tomando fecha de modificación");
+                                string CrearCarpeta = fi.LastWriteTime.ToString("yyyy_MM_dd");
+                                if (!System.IO.Directory.Exists(CrearCarpeta))
+                                {
+                                    System.IO.Directory.CreateDirectory(CrearCarpeta);
+                                    System.IO.File.Move(item, CrearCarpeta + "/" + item);
+                                }
+                                else
+                                {
+                                    System.IO.File.Move(item, CrearCarpeta + "/" + item);
+                                }
+                                break;
+                            }//if
                         }//for
                     }//if
                 }//if
